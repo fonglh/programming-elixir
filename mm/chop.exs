c6b85d2ce5cb4303beee131737d34c920e118ccf
fonglh <fonglh@gmail.com>
2015-06-27 21:51:44 +0800

    Add underscores to unused variables so elixir doesn't complain

--- mm/chop.exs
+++ mm/chop.exs
@@ -4,17 +4,17 @@ defmodule Chop do
     current_guess(div((low+high), 2), actual, low..high)
   end
 
-  defp current_guess(current, actual, low..high) when current == actual do
+  defp current_guess(current, actual, _low.._high) when current == actual do
     IO.puts actual
   end
 
-  defp current_guess(current, actual, low..high) when current < actual do
+  defp current_guess(current, actual, _low..high) when current < actual do
     next_guess = div(current + 1 + high, 2)
     IO.puts "Is it #{next_guess}?"
     current_guess(next_guess, actual, (current+1)..high)
   end
 
-  defp current_guess(current, actual, low..high) when current > actual do
+  defp current_guess(current, actual, low.._high) when current > actual do
     next_guess = div(low + current - 1, 2)
     IO.puts "Is it #{next_guess}?"
     current_guess(next_guess, actual, low..(current-1))
