590534506da48a612666b63cf23fdff88d228a19
fonglh <fonglh@gmail.com>
2015-07-01 22:56:35 +0800

    Implement Enum.each

--- lists/myenum.exs
+++ lists/myenum.exs
@@ -3,4 +3,10 @@ defmodule MyEnum do
 
   defp _all?([], _func, value), do: value
   defp _all?([ head | tail ], func, value), do: func.(head) and _all?(tail, func, value)
+
+  def each([], _func), do: :ok
+  def each([ head | tail ], func) do
+    func.(head)
+    each(tail, func)
+  end
 end
