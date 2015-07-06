1965d8252d6ae64e9a2b18949f857f88e24fb253
fonglh <fonglh@gmail.com>
2015-07-06 17:21:22 +0800

    Apply sales tax on a list of orders
    Last exercise in Chapter 10 on Processing Collections -- Enum and Stream

---
+++ lists/taxes.exs
@@ -0,0 +1,18 @@
+tax_rates = [ NC: 0.075, TX: 0.08 ]
+orders = [
+  [ id: 123, ship_to: :NC, net_amount: 100.00 ],
+  [ id: 124, ship_to: :OK, net_amount: 35.50 ],
+  [ id: 125, ship_to: :TX, net_amount: 24.00 ],
+  [ id: 126, ship_to: :TX, net_amount: 44.80 ],
+  [ id: 127, ship_to: :NC, net_amount: 25.00 ],
+  [ id: 128, ship_to: :MA, net_amount: 10.00 ],
+  [ id: 129, ship_to: :CA, net_amount: 102.00 ],
+  [ id: 120, ship_to: :NC, net_amount: 50.00 ] ]
+
+with_tax = for [id, {_ship, ship_to}, {_amt, net_amount}] <- orders do 
+  tax_rate = Keyword.get(tax_rates, ship_to, 0)
+  total_amount = net_amount + net_amount * tax_rate
+  [id, ship_to: ship_to, net_amount: net_amount, total_amount: total_amount]
+end
+
+IO.inspect with_tax
