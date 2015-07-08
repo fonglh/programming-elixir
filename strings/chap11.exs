f71c4f2fdc46b930541ef7ff074d68c0733796e9
fonglh <fonglh@gmail.com>
2015-07-08 17:15:20 +0800

    Write calculate function
    Takes a single quoted string of the form number[+-*/]number and returns
    the result of the calculation. The individual numbers do not have
    leading plus or minus signs.

--- strings/chap11.exs
+++ strings/chap11.exs
@@ -9,5 +9,27 @@ defmodule Chap11 do
 
   # return true if parameters are anagrams
   def anagram?(word1, word2), do: word1 == Enum.reverse(word2)
+
+  # write a function that takes a single quoted string of the form
+  # number[+-*/]number and returns the result of the calculation
+  def calculate(str) do
+    {num1, remainder} = _get_num1(str, 0)
+    _op(remainder, num1)
+  end
+
+  # return the 1st number, and the rest of the string in a tuple
+  defp _get_num1([ head | tail ], num1) when head in '0123456789' do
+    _get_num1(tail, num1*10 + head-?0)  
+  end
+  defp _get_num1(tail, num1), do: {num1, tail}
+
+  defp _op([ ?+ | tail ], num1), do: num1 + _number_digits(tail, 0)
+  defp _op([ ?- | tail ], num1), do: num1 - _number_digits(tail, 0)
+  defp _op([ ?* | tail ], num1), do: num1 * _number_digits(tail, 0)
+  defp _op([ ?/ | tail ], num1), do: num1 / _number_digits(tail, 0)
+  
+  # convert a number string to a number
+  defp _number_digits([], value), do: value 
+  defp _number_digits([ head | tail ], value), do: _number_digits(tail, value*10 + head-?0)
 end
 
